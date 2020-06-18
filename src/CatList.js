347d0420e49c585b7dca1499eaa64ccf804b19e0
reddevilcero <reddevil_cero@hotmail.com>
2020-06-18 11:08:23 +0100

    applied thunk middelware

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,12 @@
 // write your CatList component here
+import React, { Component } from 'react'
+
+export default class CatList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
